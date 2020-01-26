c2b954b0f2428b2e08d5d5ddbfe3d11a35607ecc
roman <roman.richter.git.user>
2020-01-26 13:59:13 +0100

    add module services

---
+++ services/src/main/java/com/mhp/coding/challenges/dependency/Email.java
@@ -0,0 +1,8 @@
+package com.mhp.coding.challenges.dependency;
+
+import com.mhp.coding.challenges.dependency.inquiry.Inquiry;
+
+public interface Email {
+    public void sendEmail(final Inquiry inquiry);
+
+}
