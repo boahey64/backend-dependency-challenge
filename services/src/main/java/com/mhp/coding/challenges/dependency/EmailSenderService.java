3c31c3a82cd86debecd0eb2d175d8cfe68d7a83f
roman <roman.richter.git.user>
2020-01-26 15:21:00 +0100

    add dependency injection

--- services/src/main/java/com/mhp/coding/challenges/dependency/EmailSenderService.java
+++ services/src/main/java/com/mhp/coding/challenges/dependency/EmailSenderService.java
@@ -1,12 +1,14 @@
 package com.mhp.coding.challenges.dependency;
 
 import com.mhp.coding.challenges.dependency.notifications.EmailHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EmailSenderService implements EmailSender {
     private EmailHandler emailHandler;
 
+    @Autowired
     public EmailSenderService(EmailHandler emailHandler) {
         this.emailHandler = emailHandler;
     }
--- services/src/main/java/com/mhp/coding/challenges/dependency/PushNotificationService.java
+++ services/src/main/java/com/mhp/coding/challenges/dependency/PushNotificationService.java
@@ -1,12 +1,14 @@
 package com.mhp.coding.challenges.dependency;
 
 import com.mhp.coding.challenges.dependency.notifications.PushNotificationHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PushNotificationService implements PushNotificationSender {
     private PushNotificationHandler pushNotificationHandler;
 
+    @Autowired
     public PushNotificationService(PushNotificationHandler pushNotificationHandler) {
         this.pushNotificationHandler = pushNotificationHandler;
     }
