8023f9e64e534fb394cf042ff2405c1791bfd868
roman <roman.richter.git.user>
2020-01-26 15:19:51 +0100

    inject service interfaces into InquiryService

--- inquiry/src/main/java/com/mhp/coding/challenges/dependency/inquiry/InquiryService.java
+++ inquiry/src/main/java/com/mhp/coding/challenges/dependency/inquiry/InquiryService.java
@@ -1,11 +1,22 @@
 package com.mhp.coding.challenges.dependency.inquiry;
 
+import com.mhp.coding.challenges.dependency.EmailSender;
+import com.mhp.coding.challenges.dependency.PushNotificationSender;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class InquiryService {
+    private EmailSender emailSender;
+    private PushNotificationSender pushNotificationSender;
+
+    @Autowired
+    public InquiryService(EmailSender emailSender, PushNotificationSender pushNotificationSender) {
+        this.emailSender = emailSender;
+        this.pushNotificationSender = pushNotificationSender;
+    }
 
     private static final Logger LOG = LoggerFactory.getLogger(InquiryService.class);
 
